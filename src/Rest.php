9862156475e5855417451dba15fdbd6ba40d203d
Jeremy Nikolic <nikolic.jeremy@gmail.com>
2019-09-26 14:47:22 -0400

    Fix issue when X-RateLimit-Remaining is x-ratelimit-remaining
    Something may have changed in CURL or on the Teamwork end because checking for $headers['X-RateLimit-Remaining'] now throw an Undefined index exception. It appears that Header keys are now in lower case.
    
    This PR is fixing the issue by lowering all the header keys before checking for the 'x-ratelimit-remaining'. As it is stated in the HTTP spec that headers are case-insensitive so it is not certain to have it in any specifieed case I guess.

--- src/Rest.php
+++ src/Rest.php
@@ -69,8 +69,10 @@ final class Rest
             $status      = (int) curl_getinfo($ch, CURLINFO_HTTP_CODE);
             $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
             $headers     = $this->parseHeaders(substr($data, 0, $header_size));
+            $headers_lower_case = array_change_key_case($headers, CASE_LOWER);
+
             if ($status === 400 &&
-                                (int) $headers['X-RateLimit-Remaining'] === 0) {
+                                (int) $headers_lower_case['x-ratelimit-remaining'] === 0) {
                 $i ++;
                 sleep(10);
             } else {
